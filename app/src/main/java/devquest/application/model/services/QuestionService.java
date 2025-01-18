4adf016c1f72ffbeba732b9590e60d8432849f35
Matheus <maatsimeao@gmail.com>
2025-01-18 15:33:32 -0300

    feat: adicionada a chamada à API

--- app/src/main/java/devquest/application/model/services/QuestionService.java
+++ app/src/main/java/devquest/application/model/services/QuestionService.java
@@ -1,4 +1,10 @@
 package devquest.application.model.services;
 
+import devquest.application.model.dtos.responses.QuestionResponseDTO;
+import org.springframework.http.ResponseEntity;
+
 public interface QuestionService {
+
+  ResponseEntity<QuestionResponseDTO> generateQuestion(String technology);
+
 }
--- app/src/main/java/devquest/application/model/services/impl/QuestionServiceImpl.java
+++ app/src/main/java/devquest/application/model/services/impl/QuestionServiceImpl.java
@@ -1,19 +1,41 @@
 package devquest.application.model.services.impl;
 
 import devquest.application.config.PromptTemplate;
+import devquest.application.model.dtos.responses.QuestionResponseDTO;
 import devquest.application.model.repositories.QuestionRepository;
 import devquest.application.model.services.QuestionService;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class QuestionServiceImpl implements QuestionService {
 
   private QuestionRepository repository;
-  private PromptTemplate prompt;
+  private String prompt;
+  private ChatClient chatClient;
 
-  public QuestionServiceImpl(QuestionRepository questionRepository, PromptTemplate promptTemplate) {
+  public QuestionServiceImpl(QuestionRepository questionRepository,
+                             String promptTemplate,
+                             ChatClient.Builder chatClientBuilder) {
     this.repository = questionRepository;
     this.prompt = promptTemplate;
+    this.chatClient = chatClientBuilder.build();
+  }
+
+  private String formatePrompt(String technology) {
+    return this.prompt.replace("{technology}", technology);
+  }
+
+  @Override
+  public ResponseEntity<QuestionResponseDTO> generateQuestion(String technology) {
+    String formatedPrompt = this.formatePrompt(technology);
+    String response = chatClient
+            .prompt()
+            .user(formatedPrompt)
+            .call()
+            .content();
+    return null;
   }
 
 }
