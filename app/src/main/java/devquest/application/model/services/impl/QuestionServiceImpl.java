ab278916acda51053e97cf6089b080cb6cdb0026
Matheus <maatsimeao@gmail.com>
2025-01-22 20:05:04 -0300

    feat: criando a lógica para separar a questão retornada pelo Openai em enunciado, alternativas, resposta correta e justificativa. Criando também um objeto Question e salvando no banco de dados

--- app/src/main/java/devquest/application/model/services/impl/QuestionServiceImpl.java
+++ app/src/main/java/devquest/application/model/services/impl/QuestionServiceImpl.java
@@ -2,11 +2,16 @@ package devquest.application.model.services.impl;
 
 import devquest.application.enums.Technology;
 import devquest.application.model.dtos.response.QuestionResponseDTO;
+import devquest.application.model.entities.Question;
 import devquest.application.model.repositories.QuestionRepository;
 import devquest.application.model.services.QuestionService;
+import devquest.application.utilities.QuestionParser;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.Set;
 
 @Service
 public class QuestionServiceImpl implements QuestionService {
@@ -14,20 +19,25 @@ public class QuestionServiceImpl implements QuestionService {
   private QuestionRepository repository;
   private String prompt;
   private ChatClient chatClient;
+  private QuestionParser questionParser;
 
   public QuestionServiceImpl(QuestionRepository questionRepository,
                              String prompt,
-                             ChatClient.Builder chatClientBuilder) {
+                             ChatClient.Builder chatClientBuilder,
+                             QuestionParser questionParser) {
 
     this.repository = questionRepository;
     this.prompt = prompt;
     this.chatClient = chatClientBuilder.build();
+    this.questionParser = questionParser;
   }
 
   @Override
   public ResponseEntity<QuestionResponseDTO> generateQuestion(Technology technology) {
     String formattedPrompt = formatprompt(technology);
     String questionString = callOpenaiAndReturnResponse(formattedPrompt);
+    Question question = createQuestionObject(questionString, technology);
+    question = repository.save(question);
     return null;
   }
 
@@ -42,6 +52,16 @@ public class QuestionServiceImpl implements QuestionService {
             .user(formattedPrompt)
             .call()
             .content();
+  }
+
+  private Question createQuestionObject(String questionString, Technology technology) {
+    return Question.builder()
+            .technology(technology)
+            .text(questionParser.getText(questionString))
+            .correctAnswer(questionParser.getCorrectAnswer(questionString))
+            .justification(questionParser.getJustification(questionString))
+            .createdAt(new Date())
+            .build();
   }
 
 }
