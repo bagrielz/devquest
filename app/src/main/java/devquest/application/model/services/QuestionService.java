14c40c56d747be3bfb6ebb849fb7e2440bd7497b
Matheus <maatsimeao@gmail.com>
2025-01-22 17:45:20 -0300

    feat: formatando prompt de geração de questão e chamando a API do OpenAI para gerar uma questão

--- app/src/main/java/devquest/application/model/services/QuestionService.java
+++ app/src/main/java/devquest/application/model/services/QuestionService.java
@@ -1,4 +1,11 @@
 package devquest.application.model.services;
 
+import devquest.application.enums.Technology;
+import devquest.application.model.dtos.response.QuestionResponseDTO;
+import org.springframework.http.ResponseEntity;
+
 public interface QuestionService {
+
+  ResponseEntity<QuestionResponseDTO> generateQuestion(Technology technology);
+
 }
--- app/src/main/java/devquest/application/model/services/impl/QuestionServiceImpl.java
+++ app/src/main/java/devquest/application/model/services/impl/QuestionServiceImpl.java
@@ -1,16 +1,47 @@
 package devquest.application.model.services.impl;
 
+import devquest.application.enums.Technology;
+import devquest.application.model.dtos.response.QuestionResponseDTO;
 import devquest.application.model.repositories.QuestionRepository;
 import devquest.application.model.services.QuestionService;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class QuestionServiceImpl implements QuestionService {
 
   private QuestionRepository repository;
+  private String prompt;
+  private ChatClient chatClient;
 
-  public QuestionServiceImpl(QuestionRepository questionRepository) {
+  public QuestionServiceImpl(QuestionRepository questionRepository,
+                             String prompt,
+                             ChatClient.Builder chatClientBuilder) {
+
     this.repository = questionRepository;
+    this.prompt = prompt;
+    this.chatClient = chatClientBuilder.build();
+  }
+
+  @Override
+  public ResponseEntity<QuestionResponseDTO> generateQuestion(Technology technology) {
+    String formattedPrompt = formatprompt(technology);
+    String questionString = callOpenaiAndReturnResponse(formattedPrompt);
+    return null;
+  }
+
+  private String formatprompt(Technology technology) {
+    String promptCloned = prompt;
+    return String.format(promptCloned, technology.getTechnology());
+  }
+
+  private String callOpenaiAndReturnResponse(String formattedPrompt) {
+    return chatClient
+            .prompt()
+            .user(formattedPrompt)
+            .call()
+            .content();
   }
 
 }
