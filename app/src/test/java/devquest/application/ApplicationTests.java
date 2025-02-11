f5cba5081076e9419333c0be25236448dbd5284a
Matheus <maatsimeao@gmail.com>
2025-02-11 14:35:06 -0300

    feat: Testando método main

--- app/src/test/java/devquest/application/ApplicationTests.java
+++ app/src/test/java/devquest/application/ApplicationTests.java
@@ -4,12 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
-@Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class ApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void main() {
+		Application.main(new String[] {});
 	}
 
 }
--- app/src/test/java/devquest/application/TestApplication.java
+++ app/src/test/java/devquest/application/TestApplication.java
@@ -1,11 +1,13 @@
 package devquest.application;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
 
+@TestConfiguration(proxyBeanMethods = false)
 public class TestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.from(Application::main).with(TestcontainersConfiguration.class).run(args);
+		SpringApplication.from(Application::main).with(TestApplication.class).run(args);
 	}
 
 }
