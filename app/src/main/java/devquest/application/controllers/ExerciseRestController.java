dd8f06dbb006070d5a8c97b9a7f4fe6d8caac5fb
Matheus <maatsimeao@gmail.com>
2025-02-12 08:50:20 -0300

    docs: Documentando endpoint generateExercise do ExerciseController

--- app/src/main/java/devquest/application/controllers/ExerciseRestController.java
+++ app/src/main/java/devquest/application/controllers/ExerciseRestController.java
@@ -4,6 +4,12 @@ import devquest.application.enums.Difficulty;
 import devquest.application.enums.Technology;
 import devquest.application.model.dtos.response.exercises.ExerciseResponseDTO;
 import devquest.application.model.services.impl.ExerciseServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/exercicios")
+@Tag(name = "Exercise Controller", description = "Controller com endpoints de gerenciamento de exercícios")
 public class ExerciseRestController {
 
   private ExerciseServiceImpl service;
@@ -20,6 +27,25 @@ public class ExerciseRestController {
     this.service = exerciseService;
   }
 
+  @Operation(summary = "Gerar novo exercício",
+    description = "Gerar um novo exercício para a tecnologia e dificuldade que o usuário passar " +
+            "no parâmetro da requisição",
+    tags = {"Gerar"},
+    responses = {
+      @ApiResponse(description = "Sucesso", responseCode = "200",
+        content = {
+          @Content(
+            mediaType = MediaType.APPLICATION_JSON_VALUE,
+            schema = @Schema(implementation = ExerciseResponseDTO.class)
+          )
+        }
+      ),
+      @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+      @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+      @ApiResponse(description = "Forbiden", responseCode = "403", content = @Content),
+      @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+    }
+  )
   @GetMapping("/gerar")
   public ResponseEntity<ExerciseResponseDTO> generateExercise(
           @RequestParam(name = "tecnologia") Technology technology,
