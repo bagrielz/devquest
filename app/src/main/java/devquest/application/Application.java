f0a0e0e5ee8d4922af70f1727d0482008d7616e4
Matheus <maatsimeao@gmail.com>
2025-02-17 13:59:58 -0300

    refactor: Ajustando importações e códigos inutilizados

--- app/src/main/java/devquest/application/Application.java
+++ app/src/main/java/devquest/application/Application.java
@@ -8,19 +8,6 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		//generateHashedPassword();
 	}
-
-//	private static void generateHashedPassword() {
-//		PasswordEncoder pbkdf2Encoder = new Pbkdf2PasswordEncoder("", 8, 185000,
-//						Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256);
-//
-//		Map<String, PasswordEncoder> encoders = new HashMap<>();
-//		encoders.put("pbkdf2", pbkdf2Encoder);
-//		DelegatingPasswordEncoder passwordEncoder = new DelegatingPasswordEncoder("pbkdf2", encoders);
-//		passwordEncoder.setDefaultPasswordEncoderForMatches(pbkdf2Encoder);
-//		System.out.println(passwordEncoder.encode("123"));
-//		System.out.println(passwordEncoder.encode("321"));
-//	}
 
 }
--- app/src/main/java/devquest/application/services/security/AuthService.java
+++ app/src/main/java/devquest/application/services/security/AuthService.java
@@ -2,7 +2,6 @@ package devquest.application.services.security;
 
 import devquest.application.model.dtos.security.AccountCredentialsDTO;
 import devquest.application.model.dtos.security.TokenDTO;
-import devquest.application.model.entities.User;
 import devquest.application.repositories.UserRepository;
 import devquest.application.security.jwt.JwtTokenProvider;
 import org.springframework.http.ResponseEntity;
@@ -10,13 +9,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class AuthService {
